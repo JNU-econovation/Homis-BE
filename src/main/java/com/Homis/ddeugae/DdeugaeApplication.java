24d846dc478b4bc00bab4b6a68b3388800ef6c79
sunju1 <ciwrssn10@naver.com>
2025-11-21 20:50:12 +0900

    Initial commit: 스프링부트 빌드 파일 업로드

---
+++ src/main/java/com/Homis/ddeugae/DdeugaeApplication.java
@@ -0,0 +1,13 @@
+package com.Homis.ddeugae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdeugaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdeugaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Homis/ddeugae/DdeugaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Homis.ddeugae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdeugaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
