247b4fe74097bb22df40d4a4a2d5c8b3e4b510cb
sunju1 <ciwrssn10@naver.com>
2025-11-28 03:16:32 +0900

    feat: NotNull validate 오류 시 출력 메시지 지정

--- src/main/java/com/Homis/ddeugae/dto/SignupDto.java
+++ src/main/java/com/Homis/ddeugae/dto/SignupDto.java
@@ -11,14 +11,16 @@ import org.hibernate.validator.constraints.Length;
 @AllArgsConstructor @NoArgsConstructor
 public class SignupDto {
 
-    @NotNull
+    @NotNull(message = "ID 값이 없습니다.")
     @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "ID는 영문과 숫자만 가능합니다.")
     @Length(min=4, max=12, message = "ID는 최소 4자, 최대 12자여야 합니다.")
     private String userName;
 
-    @NotNull    @Length(min=8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+    @NotNull(message = "비밀번호 값이 없습니다.")
+    @Length(min=8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
     private String userPassword;
 
-    @NotNull    @Length(max=10, message = "닉네임은 최대 10자입니다.")
+    @NotNull(message = "닉네임 값이 없습니다.")
+    @Length(max=10, message = "닉네임은 최대 10자입니다.")
     private String userNickname;
 }
