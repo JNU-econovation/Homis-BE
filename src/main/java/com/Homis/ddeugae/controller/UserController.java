562593eb622f2e6370eef6963199012506c07303
sunju1 <ciwrssn10@naver.com>
2025-11-28 04:01:17 +0900

    feat: 로그인 API 추가

--- src/main/java/com/Homis/ddeugae/controller/UserController.java
+++ src/main/java/com/Homis/ddeugae/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.Homis.ddeugae.controller;
 
 import com.Homis.ddeugae.dto.ApiResponse;
+import com.Homis.ddeugae.dto.LoginDto;
 import com.Homis.ddeugae.dto.SignupDto;
 import com.Homis.ddeugae.service.UserService;
 import jakarta.validation.Valid;
@@ -21,5 +22,12 @@ public class UserController {
         userService.registerUser(signupRequest);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiResponse.success("201", "회원가입 성공"));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<?>> logIn(@RequestBody @Valid LoginDto loginRequest) {
+        userService.userLogin((loginRequest));
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success("200", "로그인 성공"));
     }
 }
