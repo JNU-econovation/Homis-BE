cd659b75584f277bcc92f122720cd4f06c0043bd
sunju1 <ciwrssn10@naver.com>
2025-11-28 02:55:38 +0900

    docs: User Entity에 대한 interface 문서

---
+++ src/main/java/com/Homis/ddeugae/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.Homis.ddeugae.repository;
+
+import com.Homis.ddeugae.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String userName);
+    Optional<User> findByUserNickname(String userNickname);
+}
