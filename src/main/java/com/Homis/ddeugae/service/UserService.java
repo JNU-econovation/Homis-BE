c11eca15ce835b41d870c91bab9750ea5668d246
sunju1 <ciwrssn10@naver.com>
2025-11-28 04:01:46 +0900

    comment: 로그인 서비스 로직 과정 작성

--- src/main/java/com/Homis/ddeugae/service/UserService.java
+++ src/main/java/com/Homis/ddeugae/service/UserService.java
@@ -3,6 +3,7 @@ package com.Homis.ddeugae.service;
 import com.Homis.ddeugae.common.enumType.ErrorCode;
 import com.Homis.ddeugae.common.exception.CustomException;
 import com.Homis.ddeugae.common.util.Pbkdf2Encoder;
+import com.Homis.ddeugae.dto.LoginDto;
 import com.Homis.ddeugae.dto.SignupDto;
 import com.Homis.ddeugae.entity.User;
 import com.Homis.ddeugae.repository.UserRepository;
@@ -33,5 +34,12 @@ public class UserService {
                 .build();
 
         userRepository.save(user);
+    }
+
+    public String userLogin(LoginDto loginDto){
+        // 아이디 일치 확인
+        // 비밀번호 일치 확인 (pwdEncoder 사용)
+        // jwt 토큰 발급
+        return "jwt 토큰";
     }
 }
