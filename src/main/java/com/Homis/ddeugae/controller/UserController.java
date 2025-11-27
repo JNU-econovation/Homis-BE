fb09f915b8d25333450d32d727f06faf95cee8bf
sunju1 <ciwrssn10@naver.com>
2025-11-28 02:56:48 +0900

    feat: 회원가입 API 등록

---
+++ src/main/java/com/Homis/ddeugae/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.Homis.ddeugae.controller;
+
+import com.Homis.ddeugae.dto.ApiResponse;
+import com.Homis.ddeugae.dto.SignupDto;
+import com.Homis.ddeugae.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse<?>> signUp(@RequestBody @Valid SignupDto signupRequest ) {
+        userService.registerUser(signupRequest);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.success("201", "회원가입 성공"));
+    }
+}
