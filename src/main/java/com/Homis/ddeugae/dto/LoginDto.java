0d725b8b01e4a2c3f17fdb471c79069d8b3bbd2d
sunju1 <ciwrssn10@naver.com>
2025-11-28 03:13:31 +0900

    docs: 로그인 request body dto 파일

---
+++ src/main/java/com/Homis/ddeugae/dto/LoginDto.java
@@ -0,0 +1,12 @@
+package com.Homis.ddeugae.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDto {
+    
+}
