9f266ad7593987b47986880290aa143e7e948185
sunju1 <ciwrssn10@naver.com>
2025-11-28 03:01:00 +0900

    feat: 전체 Controller에 대한 Exception Handler 문서, custom과 Valid 오류 처리 작성

---
+++ src/main/java/com/Homis/ddeugae/common/handler/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.Homis.ddeugae.common.handler;
+
+import com.Homis.ddeugae.common.enumType.ErrorCode;
+import com.Homis.ddeugae.common.exception.CustomException;
+import com.Homis.ddeugae.dto.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    // Custom Exception들 처리
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ApiResponse<?>> customHandler(CustomException ce){
+        final ErrorCode errorCode = ce.getErrorCode();
+        final HttpStatus status = errorCode.getStatus();
+        final String code = errorCode.getCode();
+        final String message = errorCode.getMessage();
+
+        return ResponseEntity.status(status)
+                .body(ApiResponse.error(code, message));
+    }
+
+    // Valid 오류 처리 -> message 작성 내용 나오게 했음
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<?>> validHandler(MethodArgumentNotValidException me) {
+        String message = me.getBindingResult()
+                .getAllErrors().getLast().getDefaultMessage();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error("400", message));
+    }
+
+
+}
