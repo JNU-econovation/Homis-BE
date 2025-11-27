d109db826e688a5cda4ed5af92f607a1ee078eef
sunju1 <ciwrssn10@naver.com>
2025-11-28 04:11:36 +0900

    docs: jwt 토큰 발급 후 반환 데이터 dto

---
+++ src/main/java/com/Homis/ddeugae/dto/JwtTokenDto.java
@@ -0,0 +1,20 @@
+package com.Homis.ddeugae.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class JwtTokenDto {
+    private final String tokenType = "Bearer";
+
+    @NotNull(message = "액세스 토큰값이 없습니다.")
+    private final String accessToken;
+
+    @NotNull(message = "리프레시 토큰값이 없습니다.")
+    private final String refreshToken;
+
+    @NotNull(message = "사용자 닉네임 값이 없습니다.")
+    private final String userNickname;
+}
