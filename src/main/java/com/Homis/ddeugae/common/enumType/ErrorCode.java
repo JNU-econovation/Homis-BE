8acdf2398d560fbd7400bc26880429173c6708c7
sunju1 <ciwrssn10@naver.com>
2025-11-29 16:16:30 +0900

    feat: 로그인 시 발생할 수 있는 에러 추가

--- src/main/java/com/Homis/ddeugae/common/enumType/ErrorCode.java
+++ src/main/java/com/Homis/ddeugae/common/enumType/ErrorCode.java
@@ -9,6 +9,12 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     // ---400
     SAME_NAME_PASSWORD(HttpStatus.BAD_REQUEST, "001_SAME_NAME_PASSWORD", "ID와 비밀번호는 같을 수 없습니다."),
+    WRONG_PWD(HttpStatus.BAD_REQUEST, "002_WRONG_PWD", "비밀번호가 틀렸습니다."),
+
+    // ---401
+    NOT_USER(HttpStatus.UNAUTHORIZED, "101_NOT_USER", "존재하지 않는 ID의 사용자입니다."),
+    EXPIRED_ACCESS(HttpStatus.UNAUTHORIZED, "102_EXPIRED_ACCESS", "액세스토큰이 만료되었습니다. 재발급 필요"),
+    REQUIRED_RE_LOGIN(HttpStatus.UNAUTHORIZED, "103_REQUIRED_RE_LOGIN", "리프레시토큰까지 만료되었습니다. 재로그인 필요"),
 
     // ---409
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "901_DUPLICATED_USER_NAME", "이미 등록된 아이디"),
