fb177a1b3603f0d3f9bcccfa30816d64eaa752ac
sunju1 <ciwrssn10@naver.com>
2025-11-29 16:16:13 +0900

    feat: 로그인 시 각 토큰에 대한 생성 메소드 작성, 두 메소드 래핑 함수 작성

--- src/main/java/com/Homis/ddeugae/common/util/JwtUtil.java
+++ src/main/java/com/Homis/ddeugae/common/util/JwtUtil.java
@@ -20,21 +20,32 @@ public class JwtUtil {
             Keys.hmacShaKeyFor(Decoders.BASE64.decode(homisJwtKey));
 
     public String createAccessToken(String nickName){
-        String accessToken = Jwts.builder()
+        final String accessToken = Jwts.builder()
                 .claim("userNickname", nickName)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 7200000))
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .expiration(new Date(System.currentTimeMillis() + 7200000)) // 3시간 유효
+                .signWith(secretKey, SignatureAlgorithm.HS256)
                 .compact();
 
         return accessToken;
     }
 
-    public String createRefreshToken(){
-        // accesstoken처럼 만들면ㄷ 되는 걸까??
+    public String createRefreshToken(String nickName){
+        String refreshToken = Jwts.builder()
+                .claim("userNickname", nickName)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 24 * 90 * 1000L)) // 3개월 유효
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+
+        return refreshToken;
     }
 
-    public JwtTokenDto createJwtToken(String nickName){
-        // JWT dto 사용해서 return
+    public JwtTokenDto createLoginResponse(String nickName){
+        final String access = createAccessToken(nickName);
+        final String refresh = createRefreshToken(nickName);
+
+        return new JwtTokenDto(access, refresh, nickName);
     }
+
 }
