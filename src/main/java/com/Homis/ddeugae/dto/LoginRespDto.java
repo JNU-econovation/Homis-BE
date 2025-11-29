5cc32aaa17d02d7e6bc1898d5e7efa0b301c05c2
sunju1 <ciwrssn10@naver.com>
2025-11-29 16:31:45 +0900

    style: validate는 requestbody 검증을 위한 것이라 효과가 없어 삭제

--- src/main/java/com/Homis/ddeugae/dto/LoginRespDto.java
+++ src/main/java/com/Homis/ddeugae/dto/LoginRespDto.java
@@ -1,17 +1,14 @@
 package com.Homis.ddeugae.dto;
 
-import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class LoginRespDto {
     private final String tokenType = "Bearer";
 
-    @NotNull(message = "액세스 토큰값이 없습니다.")
     private final String accessToken;
 
-    @NotNull(message = "사용자 닉네임 값이 없습니다.")
     private final String userNickname;
 }
