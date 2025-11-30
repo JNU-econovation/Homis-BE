cbf6ee8cdfeb91b1c15362eb1df14dce5d545e78
sunju1 <ciwrssn10@naver.com>
2025-12-01 03:19:53 +0900

    feat: jwt 추출 함수 - claim 반환 + 사용자 작성 id가 아닌 데이터 레코드 id를 claim에 넣기

--- src/main/java/com/Homis/ddeugae/common/util/JwtUtil.java
+++ src/main/java/com/Homis/ddeugae/common/util/JwtUtil.java
@@ -1,6 +1,7 @@
 package com.Homis.ddeugae.common.util;
 
 import com.Homis.ddeugae.dto.JwtTokenDto;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -19,21 +20,21 @@ public class JwtUtil {
         this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(homisJwtKey));
     }
 
-    public String createAccessToken(String userName, String nickname){
+    public String createAccessToken(Long userDataId, String nickname){
         final String accessToken = Jwts.builder()
-                .claim("userName", userName)
+                .claim("userDataId", userDataId)
                 .claim("userNickname", nickname)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 7200000)) // 3시간 유효
+                .expiration(new Date(System.currentTimeMillis() + 7200000)) // 3시간 유효 7200000
                 .signWith(secretKey, Jwts.SIG.HS256)
                 .compact();
 
         return accessToken;
     }
 
-    public String createRefreshToken(String userName, String nickname){
+    public String createRefreshToken(Long userDataId, String nickname){
         String refreshToken = Jwts.builder()
-                .claim("userName", userName)
+                .claim("userDataId", userDataId)
                 .claim("userNickname", nickname)
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 24 * 90 * 1000L)) // 3개월 유효
@@ -43,11 +44,18 @@ public class JwtUtil {
         return refreshToken;
     }
 
-    public JwtTokenDto createLoginResponse(String userName, String nickname){
-        final String access = createAccessToken(userName, nickname);
-        final String refresh = createRefreshToken(userName, nickname);
+    public JwtTokenDto createLoginResponse(Long userDataId, String nickname){
+        final String access = createAccessToken(userDataId, nickname);
+        final String refresh = createRefreshToken(userDataId, nickname);
 
         return new JwtTokenDto(access, refresh, nickname);
+    }
+
+    // JWT 까주는 함수
+    public Claims extractToken(String token) {
+        Claims claims = Jwts.parser().verifyWith(this.secretKey).build()
+                .parseSignedClaims(token).getPayload();
+        return claims; // JWT에 집어넣은 정보 반환
     }
 
 }
