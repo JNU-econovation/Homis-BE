2561a228ee2c19a1a0ac3b2027275dcdd622a1e7
sunju1 <ciwrssn10@naver.com>
2025-12-01 03:22:56 +0900

    feat: jwt 토큰 확인 interceptor web mvc configure에 등록, auth 관련 api 외 전체 적용

--- src/main/java/com/Homis/ddeugae/common/util/AuthConfiguration.java
+++ src/main/java/com/Homis/ddeugae/common/util/AuthConfiguration.java
@@ -1,6 +1,19 @@
 package com.Homis.ddeugae.common.util;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@RequiredArgsConstructor
 public class AuthConfiguration implements WebMvcConfigurer {
+    private final AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/api/**")            // 전체 API 보호 필요
+                .excludePathPatterns("/api/auth/**");  // 근데 로그인, 회원가입 등 제외
+    }
 }
