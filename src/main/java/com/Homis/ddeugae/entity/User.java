57a96f623db411f3ef36b379138bfc7150cf410b
sunju1 <ciwrssn10@naver.com>
2025-12-01 03:24:04 +0900

    style: 데이터 레코드 id 명확하게 칼럼명 rename

--- src/main/java/com/Homis/ddeugae/entity/User.java
+++ src/main/java/com/Homis/ddeugae/entity/User.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long userDataId;
 
     @Column(nullable = false, unique = true)
     private String userName;
