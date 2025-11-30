0d448cea35c3df806b83e2c5293adedd8c9cd603
sunju1 <ciwrssn10@naver.com>
2025-12-01 03:18:23 +0900

    feat: 401 에러코드 작성

--- src/main/java/com/Homis/ddeugae/common/enumType/ErrorCode.java
+++ src/main/java/com/Homis/ddeugae/common/enumType/ErrorCode.java
@@ -12,8 +12,11 @@ public enum ErrorCode {
     NOT_USER(HttpStatus.BAD_REQUEST, "002_NOT_USER", "존재하지 않는 ID의 사용자입니다."),
 
     // ---401
-    EXPIRED_ACCESS(HttpStatus.UNAUTHORIZED, "102_EXPIRED_ACCESS", "액세스토큰이 만료되었습니다. 재발급 필요"),
-    REQUIRED_RE_LOGIN(HttpStatus.UNAUTHORIZED, "103_REQUIRED_RE_LOGIN", "리프레시토큰까지 만료되었습니다. 재로그인 필요"),
+    UNKNOWN_AUTH_ERROR(HttpStatus.UNAUTHORIZED, "100_UNKNOWN_AUTH_ERROR", "알 수 없는 사용자 인증 오류"),
+    NOT_BEARER_TOKEN(HttpStatus.UNAUTHORIZED, "101_NOT_BEARER_TOKEN", "Bearer 토큰이 없습니다."),
+    INVALID_ACCESS(HttpStatus.UNAUTHORIZED, "102_INVALID_ACCESS", "JWT 토큰 오류 발생"),
+    EXPIRED_ACCESS(HttpStatus.UNAUTHORIZED, "103_EXPIRED_ACCESS", "액세스토큰이 만료되었습니다. 재발급 필요"),
+    REQUIRED_RE_LOGIN(HttpStatus.UNAUTHORIZED, "104_REQUIRED_RE_LOGIN", "리프레시토큰까지 만료되었습니다. 재로그인 필요"),
 
     // ---409
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "901_DUPLICATED_USER_NAME", "이미 등록된 아이디"),
