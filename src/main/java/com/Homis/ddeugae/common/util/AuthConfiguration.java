6bab290e2cc52b77fd00948738bc7bfe41b69317
sunju1 <ciwrssn10@naver.com>
2025-12-01 02:56:00 +0900

    docs: interceptor 사용을 위해 WebMvcConfigurer 설정 파일 문서 생성

---
+++ src/main/java/com/Homis/ddeugae/common/util/AuthConfiguration.java
@@ -0,0 +1,6 @@
+package com.Homis.ddeugae.common.util;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class AuthConfiguration implements WebMvcConfigurer {
+}
