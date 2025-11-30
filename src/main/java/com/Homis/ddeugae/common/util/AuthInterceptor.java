d67c7fa1f9c2d4fb69c3b7fb8c0a22b83325e28b
sunju1 <ciwrssn10@naver.com>
2025-12-01 03:21:55 +0900

    feat: 액세스토큰 확인 후 servlet request에 데이터 Id, 닉네임 전달

--- src/main/java/com/Homis/ddeugae/common/util/AuthInterceptor.java
+++ src/main/java/com/Homis/ddeugae/common/util/AuthInterceptor.java
@@ -1,4 +1,57 @@
 package com.Homis.ddeugae.common.util;
 
-public class JwtFilter {
+import com.Homis.ddeugae.common.enumType.ErrorCode;
+import com.Homis.ddeugae.common.exception.CustomException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.naming.AuthenticationException;
+
+@Component
+@RequiredArgsConstructor
+public class AuthInterceptor implements HandlerInterceptor {
+    private final JwtUtil jwtUtil;
+
+    private String getBearerToken(final String bearer){
+        if (bearer == null || !bearer.startsWith("Bearer ")){
+            throw new CustomException(ErrorCode.NOT_BEARER_TOKEN);
+        }
+
+        return bearer.substring("Bearer ".length());
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
+        // 화이트리스트 URI
+        if (request.getRequestURI().equals("/api/auth/refresh")) {
+            return true; // refresh api는 accessToken 없이 통과
+        }
+
+        final String authorization = request.getHeader("Authorization");
+        final String accessToken = getBearerToken(authorization);
+
+        try{
+            Claims claim = jwtUtil.extractToken(accessToken);
+            final Long userDataId = ((Number) claim.get("userDataId")).longValue();
+            final String userNickname = claim.get("userNickname").toString();
+
+            request.setAttribute("userDataId", userDataId);
+            request.setAttribute("userNickname", userNickname);
+
+            return true;
+
+        } catch (ExpiredJwtException eje){
+            throw new CustomException(ErrorCode.EXPIRED_ACCESS);
+        } catch (JwtException je){
+            throw new CustomException(ErrorCode.INVALID_ACCESS);
+        } catch (Exception e) {
+            throw new CustomException(ErrorCode.UNKNOWN_AUTH_ERROR);
+        }
+    }
 }
