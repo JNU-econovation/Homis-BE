a027dd15ed530c724b4c5383005dd215ac1204ac
sunju1 <ciwrssn10@naver.com>
2025-12-24 03:03:45 +0900

    Revert "docs: 패키지 구조 수정"
    This reverts commit 74e1a643be2970f6c43d86b13c82601c5086c5bb.

---
+++ src/main/java/com/Homis/ddeugae/domain/Make/service/MakeService.java
@@ -0,0 +1,10 @@
+package com.Homis.ddeugae.domain.Make.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MakeService {
+    
+}
