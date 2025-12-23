fd5a14ecf3b4908a734a33cbbbfd8180e93f149b
sunju1 <ciwrssn10@naver.com>
2025-12-23 22:38:42 +0900

    feat: 도안 제작 API 틀

--- src/main/java/com/Homis/ddeugae/Make/controller/MakeController.java
+++ src/main/java/com/Homis/ddeugae/Make/controller/MakeController.java
@@ -1,6 +1,13 @@
 package com.Homis.ddeugae.Make.controller;
 
+import com.Homis.ddeugae.Make.dto.MadeUploadDto;
+import com.Homis.ddeugae.common.dto.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/design-make")
 @RequiredArgsConstructor
 public class MakeController {
-    
+
+    // 도안 제작 내용 저장 API
+    @PostMapping("/upload")
+    public ResponseEntity<ApiResponse<?>> madeUpload(@RequestBody @Valid MadeUploadDto uploadReq) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.success("201", "제작 도안 저장(업로드) 성공"));
+    }
 }
