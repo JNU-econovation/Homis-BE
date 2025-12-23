4a9224dd71a7c469a8134e971682b05feb3022db
sunju1 <ciwrssn10@naver.com>
2025-12-23 17:17:35 +0900

    docs: 필요 디렉토리 생성, Controller 문서 생성

---
+++ src/main/java/com/Homis/ddeugae/Make/controller/MakeController.java
@@ -0,0 +1,12 @@
+package com.Homis.ddeugae.Make.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/design-make")
+@RequiredArgsConstructor
+public class MakeController {
+    
+}
