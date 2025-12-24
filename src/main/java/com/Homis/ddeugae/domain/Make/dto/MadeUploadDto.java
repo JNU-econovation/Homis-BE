0ae9fe8a4d49f4676effd63ec1848e18ade14d16
sunju1 <ciwrssn10@naver.com>
2025-12-24 18:23:08 +0900

    feat: 도안명은 공백일 수 있음 -> 생성일시로 대체

--- src/main/java/com/Homis/ddeugae/domain/Make/dto/MadeUploadDto.java
+++ src/main/java/com/Homis/ddeugae/domain/Make/dto/MadeUploadDto.java
@@ -14,8 +14,7 @@ import java.util.List;
 @AllArgsConstructor @NoArgsConstructor
 public class MadeUploadDto {
 
-    @NotNull(message = "도안명은 공백일 수 없습니다.")
-    @Length(min=1, max=10, message = "도안명은 최대 10자까지만 가능합니다.")
+    @Length(max=10, message = "도안명은 최대 10자까지만 가능합니다.")
     private String madeName;
 
     @NotNull(message = "도안 규격 값이 없습니다.")
