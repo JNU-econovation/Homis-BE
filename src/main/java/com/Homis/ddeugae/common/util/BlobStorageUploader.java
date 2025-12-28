870fd2f16da04f7f75c9175b0462376683bf087a
sunju1 <ciwrssn10@naver.com>
2025-12-28 17:37:52 +0900

    feat: 파일 업로드될 컨테이너 연결, 파일 업로드 url 반환 함수

--- src/main/java/com/Homis/ddeugae/common/util/BlobStorageUploader.java
+++ src/main/java/com/Homis/ddeugae/common/util/BlobStorageUploader.java
@@ -1,7 +1,51 @@
 package com.Homis.ddeugae.common.util;
 
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.models.BlobHttpHeaders;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.util.UUID;
 
 @Component
 public class BlobStorageUploader {
+    private final BlobContainerClient containerClient; // homis-file 컨테이너와 연결될 클라이언트
+
+    // spring cloud azure를 이용해 설정해둔 이름과 일치하는 컨테이너 클라이언트 가져와 설정
+    public BlobStorageUploader(BlobContainerClient blobServiceClient,
+                               @Value("${spring.cloud.azure.storage.blob.container-name}")
+                               String containerName){
+        this.containerClient =
+                blobServiceClient.getServiceClient().getBlobContainerClient(containerName);
+    }
+
+    /**
+     * 업로드할 파일, Content-Type, 확장자 받아 Blob Storage에 저장
+     *
+     * @param data : 이미지(byte array) 등의 파일 바이트
+     * @param contentType : e.g. "image/png", "application/pdf"
+     * @param extension : 확장자명 e.g. "png", "pdf"
+     * @return : 업로드된 Blob URL -> DB에 저장될 예정
+     */
+    public String fileUpload(byte[] data, String contentType, String extension){
+        String filename = UUID.randomUUID() + "." + extension; // UUID 통해 저장될 파일명 생성 -> 충돌 방지
+
+        BlobClient blobClient = containerClient.getBlobClient(filename); // 생성한 파일명으로 Blob 객체 가져옴
+
+        // Blob에 데이터 업로드
+        blobClient.upload(
+                new ByteArrayInputStream(data),
+                data.length,
+                true // 같은 파일명일 시 덮어쓰기 (잘 없긴 하겠지만...)
+        );
+
+        // 브라우저에서 파일 직접 열 때 올바르게 처리되도록 헤더 설정
+        blobClient.setHttpHeaders(
+                new BlobHttpHeaders().setContentType(contentType)
+        );
+
+        return blobClient.getBlobUrl(); // 업로드된 Blob URL -> DB 저장, 이걸로 읽기 및 다운로드 가능!
+    }
 }
