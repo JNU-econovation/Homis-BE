5e72a553e73703a7ec9ee51d710b2244e9abf82e
sunju1 <ciwrssn10@naver.com>
2025-12-30 17:23:50 +0900

    feat: WebSnapAPI로 image URL 반환해 다운로드 후 스토리지에 업로드

--- src/main/java/com/Homis/ddeugae/domain/Make/service/MadeDesignImageService.java
+++ src/main/java/com/Homis/ddeugae/domain/Make/service/MadeDesignImageService.java
@@ -1,12 +1,42 @@
 package com.Homis.ddeugae.domain.Make.service;
 
+import com.Homis.ddeugae.common.exception.CustomException;
+import com.Homis.ddeugae.common.exception.ErrorCode;
 import com.Homis.ddeugae.common.util.BlobStorageUploader;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 @Service
 @RequiredArgsConstructor
 public class MadeDesignImageService {
     private final HtmlRenderService htmlRenderService;
     private final BlobStorageUploader blobUploader;
+
+    @Value("${websnap.auth.token}")
+    private String websnapToken;
+
+    private byte[] downloadImageBytes(String imageUrl) {
+        try (InputStream in = new URL(imageUrl).openStream()) {
+            return in.readAllBytes();
+        } catch (IOException e) {
+            throw new CustomException(ErrorCode.FAILED_DOWNLOAD_IMG);
+        }
+    }
+
+    public String createAndStoreImage(String previewUrl) {
+
+        // WebSnapAPI로 URL -> image_url 반환
+        String websnapImageUrl = htmlRenderService.captureDesign(previewUrl, websnapToken);
+
+        // 외부 image_url의 이미지 byte[] 다운로드
+        byte[] imageBytes = downloadImageBytes(websnapImageUrl);
+
+        // Azure Blob Storage에 이미지 업로드
+        return blobUploader.fileUpload(imageBytes, "image/png", "png");
+    }
 }
