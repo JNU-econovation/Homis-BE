72af5e518e2490114775f0484234c3fab6cfc45f
sunju1 <ciwrssn10@naver.com>
2025-12-30 16:30:14 +0900

    feat: 외부 API를 통해 preview url -> 이미지 url 받기

--- src/main/java/com/Homis/ddeugae/domain/Make/service/HtmlRenderService.java
+++ src/main/java/com/Homis/ddeugae/domain/Make/service/HtmlRenderService.java
@@ -1,4 +1,35 @@
 package com.Homis.ddeugae.domain.Make.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
 public class HtmlRenderService {
+    
+    private final RestClient restClient;
+
+    public HtmlRenderService(RestClient.Builder builder){
+        this.restClient = builder.build();
+    }
+
+    private static record WebSnapResponse(String status, String image_url) {}
+
+    /**
+     * WebSnapAPI 호출 후 image_url을 반환
+     *
+     * @param targetUrl : 프론트에서 요청하며 보낸 preview URL
+     * @param authToken : WebSnapAPI 인증 토큰
+     * @return : WebSnapAPI가 생성한 PNG 이미지 URL
+     */
+    public String captureDesign(String targetUrl, String authToken){
+        return restClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .scheme("https").host("websnapapi.com").path("/v1/")
+                        .queryParam("auth", authToken)
+                        .queryParam("url", targetUrl)
+                        .queryParam("fullsize", true) //웹 페이지 전체 스크린샷
+                        .build()
+                ).retrieve().body(WebSnapResponse.class)
+                .image_url(); // 반환된 이미지 url
+    }
 }
