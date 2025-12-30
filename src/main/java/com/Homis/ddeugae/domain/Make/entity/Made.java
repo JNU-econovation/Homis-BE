fd1219bfe6ef6c900e5382e1815315ce08f74f3e
sunju1 <ciwrssn10@naver.com>
2025-12-30 14:20:36 +0900

    feat: 도안 제작 게시글 entity column 정의

--- src/main/java/com/Homis/ddeugae/domain/Make/entity/Made.java
+++ src/main/java/com/Homis/ddeugae/domain/Make/entity/Made.java
@@ -1,4 +1,38 @@
 package com.Homis.ddeugae.domain.Make.entity;
 
+import com.Homis.ddeugae.domain.User.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Made {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long madeDataId;
+
+    @Column(nullable = false)
+    private String madeName;
+
+    @Column(nullable = false)
+    private Integer madeSize;
+
+    @Column
+    private String madeImgUrl;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String madeDetail;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "maker_data_id", referencedColumnName = "user_data_id")
+    private User user;   // User.java의 userDataId와 FK
 }
