0ed18c6ea55f6489c6b9020e91239f06ae962376
sunju1 <ciwrssn10@naver.com>
2025-12-30 17:31:50 +0900

    feat: db에 저장할 제작 도안 이미지 url 생성

--- src/main/java/com/Homis/ddeugae/domain/Make/service/MakeService.java
+++ src/main/java/com/Homis/ddeugae/domain/Make/service/MakeService.java
@@ -9,6 +9,8 @@ import java.time.LocalDateTime;
 @Service
 @RequiredArgsConstructor
 public class MakeService {
+    private final MadeDesignImageService madeDesignImageService;
+
     public Long uploadMadeDesign(MadeUploadReq uploadReq){
         LocalDateTime requested_at = LocalDateTime.now(); // 타임스탬프 안 찍는 대신 요청 시점 기준으로 기록
         
@@ -17,6 +19,9 @@ public class MakeService {
         }
         
         // 페이지 url -> 이미지 url -> 다운로드 -> blob storage 업로드 -> url (db 저장 예정)
+        String target_url = uploadReq.getDesignPreviewUrl();
+        String design_image_url = madeDesignImageService.createAndStoreImage(target_url);
+
         
     }
 }
