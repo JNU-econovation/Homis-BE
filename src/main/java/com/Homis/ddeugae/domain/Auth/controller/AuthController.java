3d10428f389463caab878b7f261ae77a1c4dae4d
sunju1 <ciwrssn10@naver.com>
2025-12-31 23:29:58 +0900

    docs: 배포 브랜치이므로 httpOnly 쿠키의 secure true로 변경

--- src/main/java/com/Homis/ddeugae/domain/Auth/controller/AuthController.java
+++ src/main/java/com/Homis/ddeugae/domain/Auth/controller/AuthController.java
@@ -36,7 +36,7 @@ public class AuthController {
         LoginResp loginData = new LoginResp(jwtToken.getAccessToken(), jwtToken.getUserNickname());
 
         ResponseCookie cookie = ResponseCookie.from("refreshToken", jwtToken.getRefreshToken())
-                .httpOnly(true).secure(false).sameSite("None") // TODO - 배포 시 true 변경
+                .httpOnly(true).secure(true).sameSite("None")
                 .path("/api/auth/refresh")
                 .maxAge(60 * 60 * 24 * 90)  // 리프레시 토큰은 3개월 유효
                 .build();
@@ -52,7 +52,7 @@ public class AuthController {
     @PostMapping("/logout")
     public ResponseEntity<ApiResponse<?>> logOut(){
         ResponseCookie cookie = ResponseCookie.from("refreshToken", "")
-                .httpOnly(true).secure(false).sameSite("None") // TODO - 배포 시 true 변경
+                .httpOnly(true).secure(true).sameSite("None")
                 .path("/api/auth/refresh")
                 .maxAge(0)  // 리프레시 토큰은 3개월 유효
                 .build();
