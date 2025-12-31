db0354b2a3cc7f821b1a2e2ac73912f8cddeb0a9
sunju1 <ciwrssn10@naver.com>
2025-12-31 22:45:50 +0900

    docs: dto에서 String 타입 필드 공백 validation은 NotBlank로 잡도록 수정

--- src/main/java/com/Homis/ddeugae/domain/Auth/dto/LoginReq.java
+++ src/main/java/com/Homis/ddeugae/domain/Auth/dto/LoginReq.java
@@ -1,5 +1,6 @@
 package com.Homis.ddeugae.domain.Auth.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,8 +10,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginReq {
-    @NotNull(message = "ID 입력값이 없습니다.")
+    @NotBlank(message = "ID 입력값이 없습니다.")
     private String userName;
-    @NotNull(message = "비밀번호 입력값이 없습니다.")
+    @NotBlank(message = "비밀번호 입력값이 없습니다.")
     private String userPassword;
 }
--- src/main/java/com/Homis/ddeugae/domain/Auth/dto/SignupReq.java
+++ src/main/java/com/Homis/ddeugae/domain/Auth/dto/SignupReq.java
@@ -1,5 +1,6 @@
 package com.Homis.ddeugae.domain.Auth.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -11,16 +12,16 @@ import org.hibernate.validator.constraints.Length;
 @AllArgsConstructor @NoArgsConstructor
 public class SignupReq {
 
-    @NotNull(message = "ID 값이 없습니다.")
+    @NotBlank(message = "ID 값이 없습니다.")
     @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "ID는 영문과 숫자만 가능합니다.")
     @Length(min=4, max=12, message = "ID는 최소 4자, 최대 12자여야 합니다.")
     private String userName;
 
-    @NotNull(message = "비밀번호 값이 없습니다.")
+    @NotBlank(message = "비밀번호 값이 없습니다.")
     @Length(min=8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
     private String userPassword;
 
-    @NotNull(message = "닉네임 값이 없습니다.")
+    @NotBlank(message = "닉네임 값이 없습니다.")
     @Length(max=10, message = "닉네임은 최대 10자입니다.")
     private String userNickname;
 }
