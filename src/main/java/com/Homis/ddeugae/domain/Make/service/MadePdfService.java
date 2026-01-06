c7ba72822d37759dc44a8744acaab523f6552b48
sunju1 <ciwrssn10@naver.com>
2026-01-06 18:10:28 +0900

    feat: pdf 생성 및 업로드 후 db에 저장할 pdf 파일의 url 반환 메소드

--- src/main/java/com/Homis/ddeugae/domain/Make/service/MadePdfService.java
+++ src/main/java/com/Homis/ddeugae/domain/Make/service/MadePdfService.java
@@ -1,12 +1,32 @@
 package com.Homis.ddeugae.domain.Make.service;
 
 
+import com.Homis.ddeugae.common.util.BlobStorageUploader;
 import lombok.RequiredArgsConstructor;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MadePdfService {
-    
+    private final BlobStorageUploader blobUploader;
+
+    public String createAndStorePdf(String madeImgUrl, String madeDetail){
+        PDDocument doc = new PDDocument();
+
+        PDPage imgPage = new PDPage(PDRectangle.A4);
+        doc.addPage(imgPage);
+        // addMadeImgToPdf(doc, imgPage, madeImgUrl);
+
+        PDPage detailPage = new PDPage(PDRectangle.A4);
+        doc.addPage(detailPage);
+        // addMadeDetailToPdf(doc, detailPage, madeDetail);
+
+        //byte[] pdfBytes = loadPdfBytes(doc);
+
+        //return blobUploader.fileUpload(pdfBytes, "application/pdf", "pdf");
+    }
 }
