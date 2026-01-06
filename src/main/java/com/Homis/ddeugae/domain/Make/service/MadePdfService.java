31bb7ba7c38222bbdb27165b392396a6ab95e897
sunju1 <ciwrssn10@naver.com>
2026-01-06 18:18:24 +0900

    feat: 도안 제작 pdf 파일 생성 - 두 번째 페이지부터 도안 상세 설명 저장, TODO - 내용 작성 메소드 생성 후 사용 + 도안 내용 없을 시 처리 로직 작성

--- src/main/java/com/Homis/ddeugae/domain/Make/service/MadePdfService.java
+++ src/main/java/com/Homis/ddeugae/domain/Make/service/MadePdfService.java
@@ -2,15 +2,19 @@ package com.Homis.ddeugae.domain.Make.service;
 
 
 import com.Homis.ddeugae.common.util.BlobStorageUploader;
+import com.Homis.ddeugae.domain.Make.util.ContentStreamForText;
 import lombok.RequiredArgsConstructor;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.IOException;
 
 @Service
@@ -31,7 +35,24 @@ public class MadePdfService {
         }
 
     }
-    
+
+    private void addMadeDetailToPdf(PDDocument doc, PDPage page, String madeDetail){
+        try {
+            PDPageContentStream pageContentStream = new PDPageContentStream(doc, page, PDPageContentStream.AppendMode.APPEND, true, true);
+            PDFont font = PDType0Font.load(doc,
+                    new File("C:\\Users\\user\\Homis-BE\\ddeugae\\src\\main\\resources\\fonts\\Pretendard-Medium.ttf"));
+
+            ContentStreamForText contentStreamForText = new ContentStreamForText(pageContentStream, font);
+
+            // TODO: PDF 파일에 madeDetail 내용 작성
+
+            contentStreamForText.close();
+
+        } catch (IOException ie) {
+            throw new RuntimeException(ie);
+        }
+    }
+
     public String createAndStorePdf(String madeImgUrl, String madeDetail){
         PDDocument doc = new PDDocument();
 
@@ -41,7 +62,7 @@ public class MadePdfService {
 
         PDPage detailPage = new PDPage(PDRectangle.A4);
         doc.addPage(detailPage);
-        // addMadeDetailToPdf(doc, detailPage, madeDetail);
+        addMadeDetailToPdf(doc, detailPage, madeDetail);
 
         //byte[] pdfBytes = loadPdfBytes(doc);
 
