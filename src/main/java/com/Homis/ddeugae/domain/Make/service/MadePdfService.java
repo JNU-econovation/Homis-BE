7477027955632bbbd2afef902cce77eae6782f5f
sunju1 <ciwrssn10@naver.com>
2026-01-06 18:15:22 +0900

    feat: 도안 제작 pdf 파일 생성 - 첫 페이지에 도안 이미지 저장, TODO - 이미지 규격 맞추기

--- src/main/java/com/Homis/ddeugae/domain/Make/service/MadePdfService.java
+++ src/main/java/com/Homis/ddeugae/domain/Make/service/MadePdfService.java
@@ -6,20 +6,38 @@ import lombok.RequiredArgsConstructor;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 public class MadePdfService {
     private final BlobStorageUploader blobUploader;
 
+    private void addMadeImgToPdf(PDDocument doc, PDPage page, String madeImgUrl){
+        try{
+            PDImageXObject madeImg = PDImageXObject.createFromFile(madeImgUrl, doc);
+            PDPageContentStream contentStream = new PDPageContentStream(doc, page);
+            contentStream.drawImage(madeImg, 50, 750, 595, 842); // TODO: width, height 조정 필요
+
+            contentStream.close();
+
+        } catch (IOException ie){
+            throw new RuntimeException(ie);
+        }
+
+    }
+    
     public String createAndStorePdf(String madeImgUrl, String madeDetail){
         PDDocument doc = new PDDocument();
 
         PDPage imgPage = new PDPage(PDRectangle.A4);
         doc.addPage(imgPage);
-        // addMadeImgToPdf(doc, imgPage, madeImgUrl);
+        addMadeImgToPdf(doc, imgPage, madeImgUrl);
 
         PDPage detailPage = new PDPage(PDRectangle.A4);
         doc.addPage(detailPage);
