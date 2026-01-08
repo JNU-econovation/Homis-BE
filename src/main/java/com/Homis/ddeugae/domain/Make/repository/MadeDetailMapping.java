5825b87b5ca7f793e1db8ee64492239d082e4cc2
sunju1 <ciwrssn10@naver.com>
2026-01-09 05:06:04 +0900

    docs: 제작한 도안 상세페이지 내용 가져올 메소드 작성

--- src/main/java/com/Homis/ddeugae/domain/Make/repository/MadeDetailMapping.java
+++ src/main/java/com/Homis/ddeugae/domain/Make/repository/MadeDetailMapping.java
@@ -1,4 +1,9 @@
 package com.Homis.ddeugae.domain.Make.repository;
 
 public interface MadeDetailMapping {
+    long getMadeDataId();
+    String getMadeName();
+    String getMadeImgUrl();
+    String getMadePdfUrl();
+    String getMadeDetail();
 }
