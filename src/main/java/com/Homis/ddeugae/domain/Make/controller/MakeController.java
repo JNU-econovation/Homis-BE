b5627c55658ac5b79c41238f99bbcce2aa206d3c
sunju1 <ciwrssn10@naver.com>
2026-01-10 21:40:06 +0900

    fix: 로깅 구현체 생성에 대해 통일

--- src/main/java/com/Homis/ddeugae/domain/Make/controller/MakeController.java
+++ src/main/java/com/Homis/ddeugae/domain/Make/controller/MakeController.java
@@ -13,8 +13,7 @@ import com.Homis.ddeugae.domain.Make.service.MakeService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -28,11 +27,11 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/design-make")
 @RequiredArgsConstructor
 public class MakeController {
-    private static final Logger log = LoggerFactory.getLogger(MakeController.class);
     private final MakeService makeService;
     private final BlobStorageManager blobStorageManager;
 
