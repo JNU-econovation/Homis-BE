5cef25418f4f955f56eed4009385b23fa21c227e
sunju1 <ciwrssn10@naver.com>
2026-01-10 20:45:27 +0900

    docs: 다운로드 받을 파일 종류 필드 추가

--- src/main/java/com/Homis/ddeugae/domain/Make/dto/MadeFileDownloadReq.java
+++ src/main/java/com/Homis/ddeugae/domain/Make/dto/MadeFileDownloadReq.java
@@ -1,5 +1,6 @@
 package com.Homis.ddeugae.domain.Make.dto;
 
+import com.Homis.ddeugae.common.enumType.FileType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -14,4 +15,6 @@ public class MadeFileDownloadReq {
     private Long madeDataId;
     @NotBlank(message = "다운로드 받을 도안의 이름이 없습니다.")
     private String madeName;
+    @NotNull(message = "다운로드 받을 파일의 종류를 지정하지 않았습니다.")
+    private FileType fileType;
 }
