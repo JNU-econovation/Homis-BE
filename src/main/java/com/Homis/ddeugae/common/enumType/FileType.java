0a721ebafcdcc630177fdafa2adfe9578f796995
sunju1 <ciwrssn10@naver.com>
2026-01-10 21:06:08 +0900

    docs: 굳이 extension 필드 필요없을 것 같아서 생략

--- src/main/java/com/Homis/ddeugae/common/enumType/FileType.java
+++ src/main/java/com/Homis/ddeugae/common/enumType/FileType.java
@@ -1,13 +1,6 @@
 package com.Homis.ddeugae.common.enumType;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public enum FileType {
-    IMG("png"),
-    PDF("pdf");
-
-    private final String extension;
+    IMG,
+    PDF;
 }
