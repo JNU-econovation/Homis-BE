198ff06d31aceaddb51c2b2725d58135ba71ba01
sunju1 <ciwrssn10@naver.com>
2026-01-10 17:26:13 +0900

    docs: 파일 다운로드 request body valid 확인용 dto

---
+++ src/main/java/com/Homis/ddeugae/domain/Download/dto/fileDownloadReq.java
@@ -0,0 +1,17 @@
+package com.Homis.ddeugae.domain.Download.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class fileDownloadReq {
+    @NotNull(message = "다운로드 받을 파일의 게시글 ID가 없습니다.")
+    private Long postDataId;
+    @NotBlank(message = "다운로드 받을 파일의 URL이 없습니다.")
+    private String fileUrl;
+}
