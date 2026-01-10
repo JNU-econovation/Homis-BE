760f18ca297bba0b98dd36ccfe40a7dcb011996b
sunju1 <ciwrssn10@naver.com>
2026-01-10 17:50:26 +0900

    feat: [도안 제작] 이미지 파일 다운로드 API

--- src/main/java/com/Homis/ddeugae/domain/Download/controller/FileDownloadController.java
+++ src/main/java/com/Homis/ddeugae/domain/Download/controller/FileDownloadController.java
@@ -1,4 +1,29 @@
 package com.Homis.ddeugae.domain.Download.controller;
 
+import com.Homis.ddeugae.common.dto.ApiResponse;
+import com.Homis.ddeugae.domain.Download.dto.FileDownloadReq;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/download")
+@RequiredArgsConstructor
 public class FileDownloadController {
+
+    // [도안 제작] 이미지 파일 다운로드
+    @PostMapping("/img")
+    public ResponseEntity<ApiResponse<?>> downloadImgFile(
+            HttpServletRequest request,
+            @RequestBody FileDownloadReq downloadReq){
+        Long userDataId = (Long) request.getAttribute("userDataId");
+        Long madeDataId = downloadReq.getPostDataId();
+        String blobUrl = downloadReq.getFileUrl();
+
+        // TODO : 이미지 파일 다운로드 스트림 가져와서 반환 (방식은 고민중,,,)
+    }
 }
