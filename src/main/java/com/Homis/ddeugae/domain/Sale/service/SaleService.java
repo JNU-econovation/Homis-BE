2c3027b6cb7b989b3c0d032a9d07506bbdcbe56b
sunju1 <ciwrssn10@naver.com>
2026-01-12 20:33:43 +0900

    docs: sale domain service class

---
+++ src/main/java/com/Homis/ddeugae/domain/Sale/service/SaleService.java
@@ -0,0 +1,9 @@
+package com.Homis.ddeugae.domain.Sale.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SaleService {
+}
