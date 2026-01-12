4738f1711f7938aa80a51ed71a490ba020438b3c
sunju1 <ciwrssn10@naver.com>
2026-01-12 18:55:40 +0900

    docs: 도안명 최대 길이 변경

--- src/main/java/com/Homis/ddeugae/domain/Make/dto/MadeUploadReq.java
+++ src/main/java/com/Homis/ddeugae/domain/Make/dto/MadeUploadReq.java
@@ -13,7 +13,7 @@ import org.hibernate.validator.constraints.Length;
 @AllArgsConstructor @NoArgsConstructor
 public class MadeUploadReq {
 
-    @Length(max=10, message = "도안명은 최대 10자까지만 가능합니다.")
+    @Length(max=16, message = "도안명은 최대 16자까지만 가능합니다.")
     private String madeName;
 
     @NotNull(message = "도안 규격 값이 없습니다.")
