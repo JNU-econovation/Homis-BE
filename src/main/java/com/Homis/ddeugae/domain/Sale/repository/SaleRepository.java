3b80030fff13e279816c2b3e74c5e2a4bde32989
sunju1 <ciwrssn10@naver.com>
2026-01-12 22:27:59 +0900

    docs: sale entity에 대한 repository

--- src/main/java/com/Homis/ddeugae/domain/Sale/repository/SaleRepository.java
+++ src/main/java/com/Homis/ddeugae/domain/Sale/repository/SaleRepository.java
@@ -1,4 +1,7 @@
 package com.Homis.ddeugae.domain.Sale.repository;
 
-public class SaleRepository {
+import com.Homis.ddeugae.domain.Sale.entity.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
 }
