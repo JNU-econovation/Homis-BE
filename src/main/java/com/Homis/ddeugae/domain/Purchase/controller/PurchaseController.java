68a14583efe9afe4164ee8cf936a8c2dcc4d7c05
sunju1 <ciwrssn10@naver.com>
2026-01-13 23:18:53 +0900

    docs: 구매 도메인 API controller

---
+++ src/main/java/com/Homis/ddeugae/domain/Purchase/controller/PurchaseController.java
@@ -0,0 +1,11 @@
+package com.Homis.ddeugae.domain.Purchase.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/purchase")
+@RequiredArgsConstructor
+public class PurchaseController {
+}
