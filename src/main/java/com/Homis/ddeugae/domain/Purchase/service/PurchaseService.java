d0015ebd2966bb0dfcc55012ddad02bbf3bafbec
sunju1 <ciwrssn10@naver.com>
2026-01-14 21:17:57 +0900

    docs: 구매 관련 service 로직 클래스

---
+++ src/main/java/com/Homis/ddeugae/domain/Purchase/service/PurchaseService.java
@@ -0,0 +1,10 @@
+package com.Homis.ddeugae.domain.Purchase.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PurchaseService {
+    
+}
