9d1b0b81b48b87fbc23e6f44372c39b22c6c7bc9
sunju1 <ciwrssn10@naver.com>
2026-01-14 21:14:42 +0900

    feat: 조회용 필드 이용해 특정 게시물에 대한 특정 사용자 구매 이력 확인 메소드

--- src/main/java/com/Homis/ddeugae/domain/Purchase/service/PurchaseRepository.java
+++ src/main/java/com/Homis/ddeugae/domain/Purchase/service/PurchaseRepository.java
@@ -4,4 +4,5 @@ import com.Homis.ddeugae.domain.Purchase.entity.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
+    boolean existsBySalePostIdAndPurchaserDataId(Long salePostId, Long purchaserDataId);
 }
