834ab1324341ee0d0f304bdcda592416081f5aa5
sunju1 <ciwrssn10@naver.com>
2026-01-14 23:54:08 +0900

    fix: JSON 직렬화 위해 getter 추가

--- src/main/java/com/Homis/ddeugae/domain/Main/MainLoadResp.java
+++ src/main/java/com/Homis/ddeugae/domain/Main/MainLoadResp.java
@@ -4,12 +4,15 @@ import com.Homis.ddeugae.domain.Make.repository.MadePreviewMapping;
 import com.Homis.ddeugae.domain.Purchase.repository.PurchasePreviewMapping;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Builder
-@AllArgsConstructor
+@Getter
+@AllArgsConstructor @NoArgsConstructor
 public class MainLoadResp {
-    private final List<MadePreviewMapping> madePreview;
-    private final List<PurchasePreviewMapping> purchasePreview;
+    private List<MadePreviewMapping> madePreview;
+    private List<PurchasePreviewMapping> purchasePreview;
 }
