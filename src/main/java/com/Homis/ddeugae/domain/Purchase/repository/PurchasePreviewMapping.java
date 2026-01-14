eb079e0c4c69598665d8095a479370885c8f0baa
sunju1 <ciwrssn10@naver.com>
2026-01-14 22:17:23 +0900

    docs: 도안 구매 미리보기 반환 mapping interface

---
+++ src/main/java/com/Homis/ddeugae/domain/Purchase/repository/PurchasePreviewMapping.java
@@ -0,0 +1,8 @@
+package com.Homis.ddeugae.domain.Purchase.repository;
+
+public interface PurchasePreviewMapping {
+    Long getSalePostId();
+    String getSaleThumbnailImgUrl();
+    String getSaleName();
+    String getSalerNickname();
+}
