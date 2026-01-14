f9542f112780e7b5f62890da86567376e0a76478
sunju1 <ciwrssn10@naver.com>
2026-01-15 00:18:25 +0900

    docs: 생성일, 구매일 반환 추가

--- src/main/java/com/Homis/ddeugae/domain/Make/repository/MadePreviewMapping.java
+++ src/main/java/com/Homis/ddeugae/domain/Make/repository/MadePreviewMapping.java
@@ -1,7 +1,10 @@
 package com.Homis.ddeugae.domain.Make.repository;
 
+import java.time.LocalDateTime;
+
 public interface MadePreviewMapping {
     long getMadeDataId();
     String getMadeName();
     String getMadeImgUrl();
+    LocalDateTime getCreatedAt();
 }
--- src/main/java/com/Homis/ddeugae/domain/Make/repository/MadeRepository.java
+++ src/main/java/com/Homis/ddeugae/domain/Make/repository/MadeRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface MadeRepository extends JpaRepository<Made, Long> {
-    @Query(value = "SELECT made_data_id, made_name, made_img_url FROM made WHERE(maker_data_id=:user_data_id)", nativeQuery = true)
+    @Query(value = "SELECT made_data_id, made_name, made_img_url, created_at FROM made WHERE(maker_data_id=:user_data_id)", nativeQuery = true)
     List<MadePreviewMapping> findAllByMakerDataId(@Param("user_data_id") Long userDataID);
 
     @Query(value = "SELECT made_data_id, made_name, made_img_url, made_pdf_url, made_detail FROM made WHERE(made_data_id=:made_data_id)",
