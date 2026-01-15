e90f0f02a996b228f0d5dfa50707083fee4daa21
sunju1 <ciwrssn10@naver.com>
2026-01-15 23:43:14 +0900

    docs: 스토어 접속 반환 데이터에 사용자 프로필 정보 추가

--- src/main/java/com/Homis/ddeugae/domain/Store/dto/StoreLoadResp.java
+++ src/main/java/com/Homis/ddeugae/domain/Store/dto/StoreLoadResp.java
@@ -1,6 +1,7 @@
 package com.Homis.ddeugae.domain.Store.dto;
 
 import com.Homis.ddeugae.domain.Sale.repository.SaleItemsMapping;
+import com.Homis.ddeugae.domain.User.repository.UserProfileMapping;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,5 +12,6 @@ import java.util.List;
 @NoArgsConstructor @AllArgsConstructor
 public class StoreLoadResp {
     List<SaleItemsMapping> salerStoreItems;
+    UserProfileMapping salerProfile;
     boolean myStore;
 }
