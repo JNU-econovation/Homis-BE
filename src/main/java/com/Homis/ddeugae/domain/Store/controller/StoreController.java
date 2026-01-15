ed33cfea52ce7b002895ee05b2885913a42ab38a
sunju1 <ciwrssn10@naver.com>
2026-01-15 23:30:29 +0900

    feat: 닉네임으로 스토어 접속하기 API 구현

--- src/main/java/com/Homis/ddeugae/domain/Store/controller/StoreController.java
+++ src/main/java/com/Homis/ddeugae/domain/Store/controller/StoreController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,5 +25,16 @@ public class StoreController {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ApiResponse.success("200", "내 스토어 미리보기 리스트 반환 성공",
                         storeService.loadMyPagePreview(userDataId)));
+    }
+
+    // 닉네임으로 스토어 접속 (나 or 다른 사용자)
+    @GetMapping("load")
+    public ResponseEntity<ApiResponse<?>> loadStore(
+            HttpServletRequest request, @RequestParam(name = "salerNickname") String nickname){
+        Long userDataId = (Long) request.getAttribute("userDataId");
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success("200", "스토어 접속 및 미리보기 리스트 반환 성공",
+                        storeService.loadStorePreview(userDataId, nickname)));
     }
 }
