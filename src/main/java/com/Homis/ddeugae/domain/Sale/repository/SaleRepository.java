0cd5f22a219caace725694da6a66fa727049038b
sunju1 <ciwrssn10@naver.com>
2026-01-15 17:02:02 +0900

    feat:LIKE %keyword%로 상품명, 닉네임에 대한 결과로 미리보기 매핑 반환 메소드

--- src/main/java/com/Homis/ddeugae/domain/Sale/repository/SaleRepository.java
+++ src/main/java/com/Homis/ddeugae/domain/Sale/repository/SaleRepository.java
@@ -28,4 +28,10 @@ FROM Sale s
 WHERE s.salePostId = :sale_post_id
 """)
     SaleDetailMapping findSaleDetail(@Param("sale_post_id") Long salePostId);
+
+    @Query(value = """
+            SELECT sale_post_id, sale_thumbnail_img_url, sale_name, saler_nickname, sale_price, created_at
+            FROM sale WHERE(deleted=false AND (sale_name LIKE :txt OR saler_nickname LIKE :txt))
+            """, nativeQuery = true)
+    List<SaleItemsMapping> searchItemsByTxt(@Param("txt") String txt); // txt는 앞뒤로 %가 붙은 채 전달돼야 함
 }
