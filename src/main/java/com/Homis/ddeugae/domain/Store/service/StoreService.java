81b309c8b3f4c4451a68ce2e088d968521e7683d
sunju1 <ciwrssn10@naver.com>
2026-01-15 23:31:21 +0900

    feat: 닉네임으로 스토어 접속 시 response body data 구성용 메소드 구현

--- src/main/java/com/Homis/ddeugae/domain/Store/service/StoreService.java
+++ src/main/java/com/Homis/ddeugae/domain/Store/service/StoreService.java
@@ -2,6 +2,8 @@ package com.Homis.ddeugae.domain.Store.service;
 
 import com.Homis.ddeugae.domain.Sale.repository.SaleItemsMapping;
 import com.Homis.ddeugae.domain.Sale.repository.SaleRepository;
+import com.Homis.ddeugae.domain.Store.dto.StoreLoadResp;
+import com.Homis.ddeugae.domain.User.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +13,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class StoreService {
     private final SaleRepository saleRepository;
+    private final UserRepository userRepository;
 
     public List<SaleItemsMapping> loadMyPagePreview(Long userDataId){
         return saleRepository.findUserItemsById(userDataId);
+    }
+
+    public StoreLoadResp loadStorePreview(Long userDataId, String salerNickname){
+        if(salerNickname.equals(userRepository.findById(userDataId).get().getUserNickname())){
+            // 내 스토어임.
+            return new StoreLoadResp(saleRepository.findUserItemsById(userDataId), true);
+        }
+
+        return new StoreLoadResp(saleRepository.findUserItemsByNickname(salerNickname), false);
     }
 }
